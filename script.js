95c077c5e1581feaad97395276d4a0b17bd5eb04
Imaad Uddin <imaaduddin27@gmail.com>
2021-12-29 18:10:39 -0600

    project complete

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 let resultsArray = [];
 let favorites = {};
 
+function showContent(page) {
+    window.scrollTo({top: 0, behavior: "instant"});
+    if (page === "results") {
+        resultsNav.classList.remove("hidden");
+        favoritesNav.classList.add("hidden");
+    } else {
+        resultsNav.classList.add("hidden");
+        favoritesNav.classList.remove("hidden");
+    }
+    loader.classList.add("hidden");
+}
+
 function createDOMNodes(page) {
     const currentArray = page === "results" ? resultsArray : Object.values(favorites);
     // console.log("Current Array", page, currentArray);
@@ -77,10 +89,13 @@ function updateDOM(page) {
     }
     imagesContainer.textContent = "";
     createDOMNodes(page);
+    showContent(page);
 }
 
 // Get 10 Images From NASA API
 async function getNasaPictures() {
+    // Show loader
+    loader.classList.remove("hidden");
     try {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
